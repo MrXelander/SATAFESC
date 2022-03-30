f5130d3558e0d84860dacfa2625a4aacce6a666d
Alex Cruz <alejandro.cruz1110@outlook.com>
2022-03-29 23:29:53 -0600

    probando conexion a bd

---
+++ src/modelo/Conexion.java
@@ -0,0 +1,32 @@
+package modelo;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author aleja
+ */
+public class Conexion {
+    private final String driver = "com.mysql.cj.jdbc.Driver";
+    private final String bd = "tienda";
+    private final String user = "root";
+    private final String password = "";
+    private final String url = "jdbc:mysql://localhost:3306/" + bd;
+    Connection con = null;
+    
+    public Connection getConexion(){
+        try{
+            Class.forName(driver);
+            con = DriverManager.getConnection(this.url, this.user, this.password);
+            
+            System.out.println("Conectado a la base de datos");
+        }catch(SQLException e){
+            System.err.println(e);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+}
---
+++ src/satafesc/App.java
@@ -0,0 +1,17 @@
+package satafesc;
+
+/**
+ *
+ * @author aleja
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        modelo.Conexion con = new modelo.Conexion();
+        con.getConexion();
+    }
+    
+}
